9979be989ccc6fd184d0c62ed0b0dcdf5cc6dca9
Deepti Badlani <deeptibadlani230533@acropolis.in>
2026-03-09 15:39:15 +0530

    add NODE_ENV production

--- legal-intake-backend/src/server.js
+++ legal-intake-backend/src/server.js
@@ -5,24 +5,23 @@ const loadSecrets = require("./utils/secretsManager");
 async function startServer() {
   try {
 
-    // 1️⃣ Load secrets from AWS
-    const secrets = await loadSecrets();
+    if (process.env.NODE_ENV === "production") {
+      const secrets = await loadSecrets();
 
-    process.env.JWT_SECRET = secrets.JWT_SECRET;
-    process.env.EMAIL_USER = secrets.EMAIL_USER;
-    process.env.EMAIL_PASS = secrets.EMAIL_PASS;
-    process.env.DB_PASS = secrets.DB_PASS;
-    process.env.POSTGRES_PASSWORD = secrets.POSTGRES_PASSWORD;
+      process.env.JWT_SECRET = secrets.JWT_SECRET;
+      process.env.EMAIL_USER = secrets.EMAIL_USER;
+      process.env.EMAIL_PASS = secrets.EMAIL_PASS;
+      process.env.DB_PASS = secrets.DB_PASS;
+      process.env.POSTGRES_PASSWORD = secrets.POSTGRES_PASSWORD;
 
-    console.log("Secrets loaded from AWS");
+      console.log("Secrets loaded from AWS");
+    }
 
-    // 2️⃣ Start database
     await db.sequelize.sync();
 
     const seedAdmin = require("./database/seeders/seedAdmin.js");
     await seedAdmin();
 
-    // 3️⃣ Start server
     await app.listen({ port: 3000, host: "0.0.0.0" });
 
     console.log("Server running");
